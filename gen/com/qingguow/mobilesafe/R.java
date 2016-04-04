0203cdee49bb867fce1ede0ca52437880b89d95d
taoshihan <630892807@qq.com>
2016-04-04 15:36:44 +0800

    Splash欢迎界面的UI

--- gen/com/qingguow/mobilesafe/R.java
+++ gen/com/qingguow/mobilesafe/R.java
@@ -12,6 +12,10 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int luncher_bg=0x7f020001;
+    }
+    public static final class id {
+        public static final int tv_splash_version=0x7f060000;
     }
     public static final class layout {
         public static final int activity_splash=0x7f030000;
--- src/com/qingguow/mobilesafe/SplashActivity.java
+++ src/com/qingguow/mobilesafe/SplashActivity.java
@@ -1,15 +1,30 @@
 package com.qingguow.mobilesafe;
 
 import android.app.Activity;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.TextView;
 
 public class SplashActivity extends Activity {
-
+	private TextView tv_splash_version;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_splash);
+		tv_splash_version=(TextView) findViewById(R.id.tv_splash_version);
+		tv_splash_version.setText("版本号"+getVersionName());
+	}
+	//获得应用版本名称
+	private String getVersionName(){
+		PackageManager pm=getPackageManager();
+		try {
+			PackageInfo info=pm.getPackageInfo(getPackageName(), 0);
+			return info.versionName;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "";
+		}
+		
 	}
 }
